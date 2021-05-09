eb18eaa32db6884bf2cce8acc4a4a31fa380e6d4
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2021-05-09 10:35:45 +0400

    HTML&CSS&BOOTSTRAP Project

---
+++ main.js
@@ -0,0 +1,15 @@
+function background() {
+    let backgrounda = document.getElementsByClassName("rowa");
+    for(let i = 0; i < backgrounda.length; i++) {
+        let element = backgrounda[i];
+        element.style.backgroundColor = "grey";
+    } 
+}
+
+function background1() {
+    let backgrounda = document.getElementsByClassName("rowa");
+    for(let i = 0; i < backgrounda.length; i++) {
+        let element = backgrounda[i];
+        element.style.backgroundColor = "rgb(241, 239, 235)";
+    } 
+}
